e1f5def348c3b5eccf448dfc9849015f45357edd
David Irizarryhernandez <dirizarryher@cs.csubak.edu>
2016-05-02 09:42:44 -0700

    Moved structs for objects into a header file for better file management, moved sprites into a function in davidH.cpp, started to add a beginning and ending menu for the game, working on adding sounds to the game in the future

---
+++ Structures.h
@@ -0,0 +1,148 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+//#include <X11/Xutil.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <X11/keysym.h>
+#include <gl/glx.h>
+#include "ppm.h"
+#include "log.h"
+#include "Structures.h"
+extern "C" {
+#include "fonts.h"
+}
+
+//macros
+#define rnd() (((Flt)rand())/(Flt)RAND_MAX)
+#define random(a) (rand()%a)
+#define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
+#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
+#define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
+#define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
+(c)[1]=(a)[1]-(b)[1]; \
+(c)[2]=(a)[2]-(b)[2]
+
+//constants
+#define WINDOW_WIDTH  2000
+#define WINDOW_HEIGHT 1000
+#define SEGMENTS 60
+#define ADJUST (3.14159 * 2.0) / (float)SEGMENTS
+#define MAX_PARTICLES 5000
+#define GRAVITY 0.1
+#define ALPHA 1
+
+//defined types
+typedef double Flt;
+typedef double Vec[3];
+typedef Flt	Matrix[4][4];
+extern int xres;
+extern int yres;
+extern Display *dpy;
+extern Window win;
+extern XEvent *e;
+
+struct Runner
+{
+	Vec pos;
+	Vec vel;
+	float angle;
+	float color[3];
+	bool damaged;
+	Runner() {
+		/*VecZero(dir);
+		pos[0] = (Flt) (xres / 2);
+		pos[1] = (Flt) (yres / 2);
+		pos[2] = 0.0f;
+		VecZero(vel);
+		angle = 0.0;
+		color[0] = 1.0;
+		color[1] = 1.0;
+		color[2] = 1.0;
+		superMode = 0;
+		damageTaken = 0;*/
+	}
+};
+
+struct Vecs
+{
+	float x, y, z;
+};
+
+struct Shape
+{
+	float width, height;
+	float radius;
+	Vecs center;
+};
+
+struct Particle
+{
+	Shape s;
+	Vecs velocity;
+};
+
+struct Obstacle
+{
+	Vec pos;
+	int nverts;
+	Flt radius;
+	Vec vert[4];
+	float angle;
+	float rotate;
+	float color[3];
+	struct Obstacle *prev;
+	struct Obstacle *next;
+	Obstacle()
+	{
+		prev = NULL;
+		next = NULL;
+	}
+};
+
+struct Game
+{
+	Shape box[5];
+	Shape circle[2];
+	Particle particle[MAX_PARTICLES];
+	int n;
+	int lastMousex;
+	int lastMousey;
+
+	Obstacle *obhead;
+	int nobstacles;
+	int gameTimer;
+	
+	bool jump;
+	bool gameOver;
+	unsigned long int score;
+
+/*	struct timespec rTimer;
+	struct timespec obsTimer;
+	struct timespec jTimer;
+	struct timespec sTimer;
+	struct timespec dTimer;
+*/
+	Game()
+	{
+		n = 0;
+		obhead = NULL;
+		nobstacles = 0;
+		gameTimer = 0;
+		score = 0;
+		jump = false;
+		gameOver = false;
+	}
+};
+
+#endif
--- davidH.cpp
+++ davidH.cpp
@@ -1,42 +1,122 @@
 // David A. Hernandez II CS335
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <unistd.h>
-#include <time.h>
-#include <math.h>
-#include <X11/Xlib.h>
-//#include <X11/Xutil.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-#include <X11/keysym.h>
-#include <GL/glx.h>
-#include "log.h"
-#include "ppm.h"
-using namespace std;
+#include "Structures.h"
+#include <cstdio>
+//#include <FMOD/fmod.h>
+//#include <FMOD/wincompat.h>
+#include <fmod.h>
+extern "C" {
+#include "fonts.h"
+}
 
-//Defined Types
-typedef double Flt;
-typedef double Vec[3];
-typedef Flt     Matrix[4][4];
+Ppmimage *runningImage = NULL;
+Ppmimage *jumpImage = NULL;
+Ppmimage *deathImage = NULL;
+GLuint runnerTexture, jumpTexture, deathTexture;
 
-typedef struct t_bigfoot {
-    Vec pos;
-    Vec vel;
-} Bigfoot;
+void getRunnerTexture(void);
+void setRunnerTexture(Game *g);
 
+void getRunnerTexture(void)
+{
+	runningImage = ppm6GetImage("./images/runner/runner_sheet2.ppm");
+	jumpImage = ppm6GetImage("./images/runner/runner_sheet2.ppm");
+	deathImage = ppm6GetImage("./images/runner/runner_sheet2.ppm");
+	runningImage = ppm6GetImage("./images/runner/runner_sheet2.ppm");
+
+	glGenTextures(1, &runnerTexture);
+	glGenTextures(1, &jumpTexture);
+	glGenTextures(1, &deathTexture);
+	
+	glBindTexture(GL_TEXTURE_2D, runnerTexture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER_GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER_GL_NEAREST);
+	glTexImage2d(GL_TEXTURE_2D, 0, 3, runningImage->w, runningImage->h,
+		0, GL_RGB, GL_UNSIGNED_BYTE, runningImage->data);
+
+	glBindTexture(GL_TEXTURE_2D, jumpTexture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER_GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER_GL_NEAREST);
+	glTexImage2d(GL_TEXTURE_2D, 0, 3, jumpImage->w, jumpImage->h,
+		0, GL_RGB, GL_UNSIGNED_BYTE, jumpImage->data);
+
+	glBindTexture(GL_TEXTURE_2D, deathTexture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER_GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER_GL_NEAREST);
+	glTexImage2d(GL_TEXTURE_2D, 0, 3, deathImage->w, deathImage->h,
+		0, GL_RGB, GL_UNSIGNED_BYTE, deathImage->data);
+
+
+}
+
+
+
+
+
+void init_sounds(void)
+{
+	if (fmod_init()) {
+		printf("ERROR");
+		return;
+	}
+	if (fmod_createsound((char *)"./sounds/untitled.mp3", 0)) {
+		printf("ERROR");
+		return;
+	}
+	if (fmod_createsound((char *)"./sounds/untitled2.mp3", 1)) {
+		printf("ERROR");
+		return;
+	}
+	fmod_setmode(0, FMOD_LOOP_NORMAL);
+	fmod_setmode(1, FMOD_LOOP_NORMAL);
+}
+
+void play_music(int a)
+{
+	fmod_playsound(a);
+}
 
 string convertImage(string filename, string path, string filetype)
 {
     string oldfile = path + filename + filetype;
-    string newfile = filename + ".ppm";
-    filename = system("convert path newfilename");
-//    system(convert  strcat(filename, ".ppm"));
+    string newfile = path + filename + ".ppm";
+
+    //convert ./images/runner/runner_sheet2.ppm ./images/runner/runner_sheet2.png
+    string newstuff =  "convert " + oldfile + " " + newfile;
+    filename = system(newstuff.data());
     return filename;
 
 }
-    
 
 
 
+
+
+
+
+
+
+bool endGame(Game *g)
+{
+	//If runner dies, game over
+	if (g->runner.dead >= 100)
+		return true;
+	else
+		return false;
+}
+
+void endMenu(Game *g)
+{
+	int yellow = 0x00ffff00;
+	int redd = 0x00ff0000;
+
+	Rect r;
+	r.bot = yres - 300;
+	r.left = 600;
+	ggprint16(&r, 50, red, "GAME OVER");
+	ggprint16(&r, 50, red, "You died!");
+	ggprint16(&r, 50, yellow, "Your Score: %i", g->score);
+	ggprint16(&r, 50, yellow, "Your Time: %i seconds", g->gameTimer); 
+	
+	r.bot = yres - 600;
+	ggprint16(&r, 50, yellow, "Press ESC to Exit");
+}
