7c9c1d7e8523d3c176147a918aef8f0a4a87b137
David Irizarryhernandez <dirizarryher@cs.csubak.edu>
2016-05-18 10:48:10 -0700

    Update source, header file, and Makefile to work openAL functionality. Added a soundboard for runner, spear, and menu sounds.

--- davidH.cpp
+++ davidH.cpp
@@ -2,184 +2,181 @@
 #include "davidH.h"
 #include </usr/include/AL/alut.h>
 
+Sound::Sound() 
+{
+    volumeFlag = false;
+    error = 0;
+}
+
+Sound::~Sound() {
+    delete [] buffers;
+    delete [] sources;
+}
+
+void Sound::get_error(int err) 
+{
+    error = err;
+}
+
+void Sound::get_flag(bool flag)
+{
+    volumeFlag = flag;
+}
+
+
+
+
 string get_ALerror(int errID)
 {
-	if (errID == AL_NO_ERROR) {
-		return "";
-	}
-	if (errID == AL_INVALID_NAME) {
-		return "Invalid name";
-	}
-	else if (errID == AL_INVALID_VALUE) {
-		return "Invalid value";
-	}
-	else if (errID == AL_INVALID_ENUM) {
-		return "Invalid enum";
-	}
-	else if (errID == AL_INVALID_OPERATION) {
-		return "Invalid operation";
-	}
-	else if (errID == AL_OUT_OF_MEMORY) {
-		return "Not enough memory";
-	}
-	return "Error undefined";
+    if (errID == AL_NO_ERROR) {
+        return "";
+    }
+    if (errID == AL_INVALID_NAME) {
+        return "Invalid name";
+    }
+    else if (errID == AL_INVALID_VALUE) {
+        return "Invalid value";
+    }
+    else if (errID == AL_INVALID_ENUM) {
+        return "Invalid enum";
+    }
+    else if (errID == AL_INVALID_OPERATION) {
+        return "Invalid operation";
+    }
+    else if (errID == AL_OUT_OF_MEMORY) {
+        return "Not enough memory";
+    }
+    return "Error undefined";
 }
 
 void init_sounds(void)
 { 
-    ALuint alSource;
-    ALuint alBuffer;
-
     alutInit(0, NULL);
-	if (alGetError() != AL_NO_ERROR) {
-		get_ALerror(error);
-		return;
-	}
-	//Clear error state
-	alGetError();
+    if ((sound.error = alGetError()) != AL_NO_ERROR) {
+        get_ALerror(sound.error);
+        return;
+    }
+    
+    //Clear error state
+    alGetError();
 
-	//Setup the Listener
-    //Forward and up vectors are used
-	//float vec[6] = {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f};
-	alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
-	alListenerfv(AL_ORIENTATION, vec);
-	alListenerf(AL_GAIN, 1.0f);
+    //Setup the initial positions for the listener position,
+    //velocity, and orientation
+    alListenerfv(AL_POSITION, listenerPos);
+    alListenerfv(AL_ORIENTATION, listenerOri);
+    alListenerfv(AL_VELOCITY, listenerVel);
+    //alListenerf(AL_GAIN, 1.0f);
 
-	//alBuffer holds the sound information
-	alBuffer = alutCreateBufferFromFile("./test.wav");
+    //Create the buffers and check for errors
+    alGenBuffers(NUM_BUFFERS, sound.buffers);
+    if ((sound.error = alGetError()) != AL_NO_ERROR) {
+        get_ALerror(sound.error);
+        return;
+    }
+    alGetError();
 
-	//alSource refers to the sound
-	//Generate a source and store it into a buffer
-	alGenSources(1, &alSource);
-	alSourcei(alSource, AL_BUFFER, alBuffer);
+    //Load the wav file
+    //buffers[] holds the sound information
+    sound.buffers[0] = alutCreateBufferFromFile("sounds/test.wav");
+    
 
-	//Set volume and pitch to normal, no looping of sound
-	alSourcef(alSource, AL_GAIN, 1.0f);
-	alSourcef(alSource, AL_PITCH, 1.0f);
-	alSourcei(alSource, AL_LOOPING, AL_FALSE);
-	if (alGetError() != AL_NO_ERROR) {
-		printf("Error: Setting Source\n");
-		return;
-	}
+    //sources refers to the sound
+    //Generate a source and store it into a buffer
+    alGenSources(NUM_SOURCES, sound.sources);
+    //alSourcei(sound.alSource, AL_BUFFER, sound.alBuffer);
+    if ((sound.error = alGetError()) != AL_NO_ERROR) {
+        get_ALerror(sound.error);
+        return;
+    }
+    alGetError();
 
-	for (int i = 0; i < 4; i++) {
-		alSourcePlay(alSource);
-		usleep(250000);
-	} 
+    //Set properties of the sound.
+    alSourcef(sound.sources[0], AL_GAIN, 1.0f);
+    alSourcef(sound.sources[0], AL_PITCH, 1.0f);
+    alSourcefv(sound.sources[0], AL_POSITION, sourcePos);
+    alSourcefv(sound.sources[0], AL_VELOCITY, sourceVel);
+    //Source is associated with its buffer
+    alSourcei(sound.sources[0], AL_BUFFER, sound.buffers[0]);
+    alSourcei(sound.sources[0], AL_LOOPING, AL_FALSE);
+
+    if ((sound.error = alGetError()) != AL_NO_ERROR) {
+        printf("Error: Setting Source\n");
+        get_ALerror(sound.error);
+        return;
+    }
+/*
+    for (int i = 0; i < 4; i++) {
+        alSourcePlay(alSource);
+        usleep(250000);
+    } 
+*/
 }
 
 void play_music(ALuint alSource)
 {
-	alSourcePlay(alSource);
+    alSourcePlay(alSource);
 }
 
 //Creates and load an array of sounds for a soundboard
-void load_soundboard(void) {	
-    alGenBuffers(NUM_BUFFERS, buffers);
-	if ((error = alGetError() != AL_NO_ERROR)) {
-		printf("alGenBuffers Error: %d",error);
-		return;
-	}
-	alGetError();
+void load_soundboard(void) 
+{	
+    sound.buffers[1] = alutCreateBufferFromFile("sounds/runner1.wav");
+    sound.buffers[2] = alutCreateBufferFromFile("sounds/runner2.wav");
+    sound.buffers[3] = alutCreateBufferFromFile("sounds/runner3.wav");
+    sound.buffers[4] = alutCreateBufferFromFile("sounds/runner4.wav");
+    sound.buffers[5] = alutCreateBufferFromFile("sounds/runner5.wav");
+    sound.buffers[6] = alutCreateBufferFromFile("sounds/runner6.wav");
+    sound.buffers[7] = alutCreateBufferFromFile("sounds/spear1.wav");
+    sound.buffers[8] = alutCreateBufferFromFile("sounds/spear2.wav");
+    sound.buffers[9] = alutCreateBufferFromFile("sounds/spear3.wav");
+    sound.buffers[10] = alutCreateBufferFromFile("sounds/spearFlurry.wav");
+    sound.buffers[11] = alutCreateBufferFromFile("sounds/spearLTR.wav");
+    sound.buffers[12] = alutCreateBufferFromFile("sounds/spearRTL.wav");
+    sound.buffers[13] = alutCreateBufferFromFile("sounds/button1.wav");
+    sound.buffers[14] = alutCreateBufferFromFile("sounds/button2.wav");
+    sound.buffers[15] = alutCreateBufferFromFile("sounds/endOfGame.wav");
+    sound.buffers[16] = alutCreateBufferFromFile("sounds/HailToTheKing.wav");
+    sound.buffers[17] = alutCreateBufferFromFile("sounds/OutOfGum.wav");
 
-	//Initializing array of sounds
-	alutLoadWAVFile("test.wav", &format, &data, &size, &freq, &loop);
-    //alutCreateBufferFromFile("./test.wav");
-	if ((error = alGetError()) != AL_NO_ERROR) {
-		printf("alutLoadWAVFile Error: %d", error);
-		//Delete buffers if they don't load
-		alDeleteBuffers(NUM_BUFFERS, buffers);
-		return;
-	}
-    alGetError();
+    
+    for (int i = 1; i < 17; i++) {
+        alSourcef(sound.sources[i], AL_GAIN, 1.0f);
+        alSourcef(sound.sources[i], AL_PITCH, 1.0f);
+        alSourcefv(sound.sources[i], AL_POSITION, sourcePos);
+        alSourcefv(sound.sources[i], AL_VELOCITY, sourceVel);
+        alSourcei(sound.sources[i], AL_BUFFER, sound.buffers[i]);
+        alSourcei(sound.sources[i], AL_LOOPING, AL_FALSE);
+    }
 
-	//Fills up buffers with data received from alutLoadWAVFile into a buffer
-	//and makes it ready to be assigned to a waiting sources and played
-	alBufferData(buffers[0], format, data, size, freq);
-	if ((error = alGetError()) != AL_NO_ERROR) {
-		printf("alBufferData buffer[0] Error: %d", error);
-		//Delete if fails
-		alDeleteBuffers(NUM_BUFFERS, buffers);
-		return;
-	}
-    alGetError();
-
-	//Deletes sound data from memory since data is now stored in the buffer
-	alutUnloadWAV(format, data, size, freq);
-	if(error != AL_NO_ERROR) {
-		printf("alutUnloadWAV Error: %d", error);
-		//Delete if fails
-		alDeleteBuffers(NUM_BUFFERS, buffers);
-		return;
-	}
-    alGetError();
-
-	//Generate the sources
-	ALuint source[NUM_SOURCES];
-	alGenSources(NUM_SOURCES, source);
-	if ((error = alGetError()) != AL_NO_ERROR) {
-		printf("alGenSources Error: %d", error);
-		return;
-	}
-    alGetError();
-
-	//Attaches buffer to the source in order to use buffer
-	//when the source is played
-	alSourcei(source[0], AL_BUFFER, buffers[0]);
-	if ((error = alGetError()) != AL_NO_ERROR) {
-		printf("alSourcei Error: %d", error);
-		return;
-	}
-    alGetError();
-
-	//Set listener for 3D sounds
-	ALvoid alListenerfv(ALenum pname, ALfloat *values);
-	alListenerfv(AL_POSITION, listenerPos);
-	alListenerfv(AL_VELOCITY, listenerVel);
-	alListenerfv(AL_DIRECTION, listenerOri);
-	//Setting the position of the source to simulate 3D sound
-	ALvoid alSourcefv(ALuint source, ALenum pname, ALfloat *values);
-	alSourcefv(source[0], AL_POSITION, sourcePos);
-	alSourcefv(source[0], AL_VELOCITY, sourceVel);
-	alSourcefv(source[0], AL_DIRECTION, sourceOri);
-
-	//Now we play the sound using:
-	//alSourcePlay(source[0]);
-	//Stopping sound:
-	//alSourceStop(souce[0]);
-	//Resetting sound to the start of the sample
-	//alSourceRewind(souce[0]);
-	//Pausing sound:
-	//alSourcePause(souce[0]);
+    //Now we play the sound using:
+    //alSourcePlay(source[0]);
+    //Stopping sound:
+    //alSourceStop(souce[0]);
+    //Resetting sound to the start of the sample
+    //alSourceRewind(souce[0]);
+    //Pausing sound:
+    //alSourcePause(souce[0]);
 }
 
-void clean_music(ALuint &alBuffer, ALuint alSource) 
+void clean_music(void) 
 {
-	//First, delete the source
-	alDeleteSources(1, &alSource);
-	//Delete the buffer
-	alDeleteBuffers(1, &alBuffer);
+    //Sound sound;
+    //Delete the soundboard source and buffer
+    alDeleteSources(NUM_SOURCES, sound.sources);
+    alDeleteBuffers(NUM_BUFFERS, sound.buffers);
 
-	//Delete the soundboard source and buffer
-	alDeleteSources(NUM_SOURCES, source);
-	alDeleteBuffers(NUM_BUFFERS, buffers);
-
-	//Close out OPAL itself
-	//Get active context
-	ALCcontext *Context = alcGetCurrentContext();
-	//Get device for active context
-	ALCdevice *Device = alcGetContextsDevice(Context);
-	//Disable Context
-	alcMakeContextCurrent(NULL);
-	//Release Context
-	alcDestroyContext(Context);
-	//Close Device
-	alcCloseDevice(Device);
-
-	//alutExit();
+    //Close out OPAL itself
+    //Get active context
+    sound.Context = alcGetCurrentContext();
+    //Get device for active context
+    sound.Device = alcGetContextsDevice(sound.Context);
+    //Disable Context
+    alcMakeContextCurrent(NULL);
+    //Release Context
+    alcDestroyContext(sound.Context);
+    //Close Device
+    alcCloseDevice(sound.Device);
 }
-
-
 
 /*
 //Converts an image with any file extension imported into 
--- davidH.h
+++ davidH.h
@@ -1,3 +1,5 @@
+#ifndef DAVID_H
+#define DAVID_H
 #include <iostream>
 #include <unistd.h>
 #include <string.h>
@@ -12,18 +14,21 @@
 #include "ppm.h"
 #include <string>
 #include </usr/include/AL/alut.h>
+#include </usr/include/AL/al.h>
 extern "C" {
 #include "fonts.h"
 }
+//#include "header.h"
+
 using namespace std;
 
 //defined types
 #define MAX_PARTICLES 5000
-#define NUM_BUFFERS 32
-#define NUM_SOURCES 32
+#define NUM_BUFFERS 50
+#define NUM_SOURCES 50
 
 struct Vecs {
-	float x, y, z;
+    float x, y, z;
 };
 
 struct Shape {
@@ -47,33 +52,69 @@ struct Game {
     int lastMousey;
 };
 
+class Sound {
+    public:
+        Sound();
+        ~Sound();
+    public:
+        ALboolean volumeFlag;
+        ALuint alSource;
+        ALuint alBuffer;
+        ALuint buffers[NUM_BUFFERS];
+        ALuint sources[NUM_SOURCES];
+        ALuint error;
 
-ALCcontext *context;
-ALCdevice *device;
-ALuint error;
-float vec[6] = {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f};
+        ALuint alSourceRunnerSprint;
+        ALuint alSourceRunnerJump;
+        ALuint alSourceRunnerPant;
+        ALuint alSourceRunnerSlide;
+        ALuint alSourceRunnerGrunt;
+        ALuint alSourceRunnerDeath;
+        ALuint alSourceSpear1;
+        ALuint alSourceSpear2;
+        ALuint alSourceSpear3;
+        ALuint alSourceSpearFlurry;
+        ALuint alSourceSpearLTR;
+        ALuint alSourceSpearRTL;
+        ALuint alSourceButton1;
+        ALuint alSourceButton2;
+        ALuint alSourceGameOver;
+        ALuint alSourceHailToTheKing;
+        ALuint alSourceOutOfGum;
 
-//Variables for soundboard array
-ALenum format;
-ALsizei size;
-ALsizei freq;
-ALboolean loop;
-ALvoid* data;
-ALfloat listenerPos[] = {0.0, 0.0, 0.0};
-ALfloat listenerVel[] = {0.0, 0.0, 0.0};
-ALfloat listenerOri[] = {0.0, 0.0, -1.0, 0.0, 1.0, 0.0};
-ALfloat sourcePos[] = {0.0, 0.0, 0.0};
-ALfloat sourceVel[] = {0.0, 0.0, 0.0};
-ALfloat sourceOri[] = {0.0, 0.0, -1.0, 0.0, 1.0, 0.0};
+        ALCcontext *Context;
+        ALCdevice *Device;
 
-ALuint buffers[NUM_BUFFERS];
-ALuint *source = new ALuint(source[0]);
+        //Variables for soundboard array
+        ALenum format;
+        ALsizei size;
+        ALsizei freq;
+        ALboolean loop;
+        ALvoid *data;
+        ALchar *str;
+
+        void get_flag(bool flag);
+        void get_error(int err);
+};
+
+Sound sound;
+float listenerPos[] = {0.0f, 0.0f, 0.0f};
+float listenerVel[] = {0.0f, 0.0f, 0.0f};
+float listenerOri[] = {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f};
+float sourcePos[] = {0.0, 0.0, 0.0};
+float sourceVel[] = {0.0, 0.0, 0.0};
+float sourceOri[] = {0.0, 0.0, -1.0, 0.0, 1.0, 0.0};
 
 void init_sounds(void);
 void load_soundboard(void);
 void play_music(ALuint, ALuint);
-void close_music(void);
+//void play_sound(ALuint);
+//void volume(void);
+void clean_music(void);
 bool endGame(Game *g);
 bool endMenu(Game *g);
 //string convertImage(string, string, string);
 string get_ALerror(int errID);
+
+
+#endif
